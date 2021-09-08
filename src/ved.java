439f97083c6288355627de5dd2504b89d1d96a79
alla2297 <alla2297@edu.easj.dk>
2021-09-08 14:19:03 +0200

    Initial commit

---
+++ src/ved.java
@@ -0,0 +1,19 @@
+public class ved {
+
+    public static void main(String[] args) {                       //1
+        zippo("rattle", 13);                            //2
+    }                                                              //
+    public static void baffle(String blimp) {                      //8
+        System.out.println(blimp);                                 //9
+        zippo("ping", -5);                              //10
+    }                                                              //
+    public static void zippo(String quince, int flag) {            //3+11
+        if (flag < 0) {                                            //4+12
+            System.out.println(quince + " zoop");                  //13
+        } else {                                                   //5
+            System.out.println("ik");                              //6
+            baffle(quince);                                        //7
+            System.out.println("boo-wa-ha-ha");                    //14
+        }
+    }
+}
